34e28fc3ec4e80359482243bdea326766f871e85
Vento <ilya.popov.sbmt@gmail.com>
2018-03-12 14:14:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+ function solver(matrix) {
+        let b
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (!matrix[i][j]) {
+                    for (let k = 1; k < 10; k++) {
+                        if (put(matrix, i, j, k)) {
+                            matrix[i][j] = k
+                            b = solver(matrix)
+                            if (b) return true
+                            matrix[i][j] = 0
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    function put(matrix, i, j, k) {
+        for (let a = 0; a < 9; a++) {
+            if (a !== i && matrix[a][j] === k) return false
+            if (a !== j && matrix[i][a] === k) return false
+        }
+        for (let a = 0; a < 3; a++) {
+            for (let b = 0; b < 3; b++) {
+                if (a !== i && b !== j && matrix[(Math.floor((i / 3)) * 3) + a][(Math.floor((j / 3)) * 3) + b] === k) return false
+            }
+        }
+        return true
+    }
+
+    solver(matrix)
+    return matrix
 }
